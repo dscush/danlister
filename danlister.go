62912d396ccbcd5c74cdc8858d1652e5d5f8369f
Dan Cusher <dan@dancusher.com>
2016-04-16 11:05:11 -0400

    Added CLI flags and matched text output to example format

--- danlister.go
+++ danlister.go
@@ -5,13 +5,34 @@ import (
   "path/filepath"
   "os"
   "strings"
+  "flag"
 )
 
 func main() {
-  root, _ := os.Getwd()
-  fmt.Println(root)
+  var root, output string
+  var help, recursive bool
+  flag.StringVar(&root, "path", "", "<path to folder, required>")
+  flag.BoolVar(&help, "help", false, "<print help>")
+  flag.BoolVar(&recursive, "recursive", false, "<when set, list files recursively.  default is off>")
+  flag.StringVar(&output, "output", "text", "<json|yaml|text, default is text>")
+  flag.Parse()
+
+  if help {
+    flag.PrintDefaults()
+    return
+  }
+  if root == "" {
+    fmt.Println("Must provide path.  Try 'danlister --help' for usage")
+    return
+  }
+  
   base_depth := strings.Count(root, "/")
-  filepath.Walk(root, func(path string, fi os.FileInfo, _ error) (err error) {
+
+  var printText = func(path string, fi os.FileInfo, _ error) (err error) {
+    if path == root {
+      fmt.Println(root)
+      return
+    }
     depth := strings.Count(path, "/") - base_depth
     for i := 0; i < depth; i ++ {
       fmt.Print("  ")
@@ -22,5 +43,10 @@ func main() {
     }
     fmt.Println(name)
     return
-  })
+  }
+
+
+
+  filepath.Walk(root, printText)
 }
+
