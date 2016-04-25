40a3a9464984e9a927b2003c7dc5f1ee9cac7482
Dan Cusher <dan@dancusher.com>
2016-04-25 14:52:22 -0400

    Implemented JSON format and completed symlink display for text format

--- danlister.go
+++ danlister.go
@@ -1,19 +1,70 @@
 package main
 
 import (
+  "os"
   "fmt"
   "path/filepath"
   "flag"
   "io/ioutil"
+  "time"
+  "encoding/json"
 )
 
+type FileInfo struct {
+  Name string
+  Size int64
+  ModTime time.Time
+  IsDir bool
+  IsLink bool
+  LinksTo string
+  Children []FileInfo
+}
+
+func makeFileInfo(file os.FileInfo, path string, recursive bool) FileInfo {
+  children := make([]FileInfo, 0)
+  if recursive && file.IsDir(){
+    children = getChildren(path, recursive)
+  }
+  isLink := file.Mode()&os.ModeSymlink == os.ModeSymlink
+  linksTo := ""
+  if isLink {
+    linksTo, _ = os.Readlink(path)
+  }
+  return FileInfo{
+    file.Name(),
+    file.Size(),
+    file.ModTime(),
+    file.IsDir(),
+    isLink,
+    linksTo,
+    children,
+  }
+}
+
+func getChildren(path string, recursive bool) []FileInfo {
+  var children []FileInfo
+  childrenFileInfo, _ := ioutil.ReadDir(path)
+  for _, f := range childrenFileInfo {
+    children = append(children, makeFileInfo(f, filepath.Join(path, f.Name()), recursive))
+  }
+  return children
+}
+
+func printTabs(depth int) {
+  for i := 0; i < depth; i ++ {
+    fmt.Print("  ")
+  }
+}
 func printText(path string, depth int, recursive bool) {
   files, _ := ioutil.ReadDir(path)
   for _, file := range files {
-    for i := 0; i < depth; i ++ {
-      fmt.Print("  ")
+    printTabs(depth)
+    if file.Mode()&os.ModeSymlink == os.ModeSymlink {
+      linksTo, _ := os.Readlink(filepath.Join(path, file.Name()))
+      fmt.Printf("%v* (%v)\n", file.Name(), linksTo)
+    } else {
+      fmt.Println(file.Name())
     }
-    fmt.Println(file.Name())
     if recursive && file.IsDir() {
       printText(filepath.Join(path, file.Name()), depth + 1, recursive)
     }
@@ -38,7 +89,17 @@ func main() {
     return
   }
 
-  fmt.Println(root)
-  printText(root, 1, recursive)
+  if output == "json" || output == "yaml" {
+    fileTree := getChildren(root, recursive)
+    if output == "json" {
+      f, _ := json.MarshalIndent(fileTree, "", "  ")
+      fmt.Println(string(f))
+    } else {
+      fmt.Println("TODO: yaml")
+    }
+  } else {
+    fmt.Println(root)
+    printText(root, 1, recursive)
+  }
 }
 
